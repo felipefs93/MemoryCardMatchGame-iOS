1f61be5b725b8d64998d9e0f7adc0aa9348c72ec
Felipe Soares <soaresfelipef@gmail.com>
2016-08-22 12:57:43 -0300

    Facebook Pop pod added to project

---
+++ Pods/Target Support Files/Pods-MemoryCardGameTests/Pods-MemoryCardGameTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MemoryCardGameTests : NSObject
+@end
+@implementation PodsDummy_Pods_MemoryCardGameTests
+@end
---
+++ Pods/Target Support Files/Pods-MemoryCardGameTests/Pods-MemoryCardGameTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_MemoryCardGameTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MemoryCardGameTestsVersionString[];
+
